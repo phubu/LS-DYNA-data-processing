df440bb84c6cf9182ca1fe75a0ec8dd82960da40
Phu Nguyen <thienphunguyen28398@gmail.com>
2021-10-07 23:49:45 +0700

    added HIC calculation, but do not calculate max HIC 3,15, 36 yet

--- matplot1.py
+++ matplot1.py
@@ -1,4 +1,4 @@
-#import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 with open("accel_time_data.txt","r") as f:
@@ -7,18 +7,45 @@ with open("accel_time_data.txt","r") as f:
 
 x = [line.split()[0] for line in lines]
 y = [line.split()[1] for line in lines]
-
 for i in range(len(x)):
     try:
         float(x[i])
         count = i
     except:
         countfinal = i+1
+#print(countfinal)
+removeX = []
+removeY = []
 for j in range(countfinal):
-    x.pop(j)
-    y.pop(j)
+    removeX.append(x[j])
+    removeY.append(y[j])
+for xr in removeX:
+    x.remove(xr)
+for yr in removeY:
+    y.remove(yr)
+x_new = list(map(float,x))
+y_new = list(map(float,y))
+
+print(x_new)
+#------------------------------------------------------------------
+# phải lay khoang HIC 3,15,36 max trong khoang thoi gian va cham 
+t1 = 10
+t2 = 27
+def a_average(t1 = float, t2 = float):
+
+    index1 = x_new.index(t1)
+    index2 = x_new.index(t2)
+    max_t = max(t1,t2)
+    print(y_new[index1:index2])
+    print(x_new[index1:index2])
+    #return (np.trapz(y_new[index1:index2], x_new[index1:index2],dx = 1 ))
+    return ((((np.trapz(y_new[index1:index2], x_new[index1:index2]))/(t2-t1-2))**2.5)*(t2-t1-2))
+
+print(a_average(t1,t2))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(x,y, c='r')
+ax.plot(x_new,y_new, c='r')
+plt.xlabel('Time (ms)')
+plt.ylabel('acceleration (g)')
 plt.show()
